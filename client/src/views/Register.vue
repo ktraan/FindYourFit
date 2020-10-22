bf2de4e7d8279c865eb73a43fd68db62dc7e172b
ktran <kevintraan@live.com>
2020-10-22 00:12:15 -0600

    User Schema and Register.vue

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,10 +1,74 @@
 <template>
-  <v-container name="register">
-    <v-row class="d-flex">
-      <div class="text-h1">Register</div>
+  <v-container>
+    <v-row class="d-flex justify-center mt-10">
+      <v-card class="pa-5" width="700" height="500">
+        <v-row class="d-flex justify-center">
+          <div class="text-h4 mt-5">Register</div>
+        </v-row>
+        <v-row class="d-flex justify-space-between  mt-10">
+          <!-- First Col -->
+          <v-col cols="6">
+            <v-text-field label="First Name" dense outlined> </v-text-field>
+            <v-text-field
+              dense
+              label="Email Address"
+              hint="eg. fyf@gmail.com"
+              outlined
+            >
+            </v-text-field>
+            <v-text-field
+              dense
+              label="Password"
+              hint="Password must have 8 characters or more."
+              outlined
+            >
+            </v-text-field>
+
+            <v-radio-group class="ml-1" label="Sex:" row dense>
+              <v-radio label="Male" color="blue darken-1" ripple></v-radio>
+              <v-radio label="Female" color="pink lighten-3" ripple></v-radio>
+              <v-radio label="Other" color="purple accent-4" ripple></v-radio>
+            </v-radio-group>
+          </v-col>
+
+          <!-- Second Col -->
+          <v-col cols="6">
+            <v-text-field dense label="Last Name" outlined></v-text-field>
+            <v-text-field
+              dense
+              label="Date of Birth"
+              hint="eg. 2020-01-30"
+              outlined
+            >
+            </v-text-field>
+            <v-text-field dense label="Confirm Password" outlined>
+            </v-text-field>
+            <v-text-field
+              dense
+              label="Location"
+              hint="eg. Edmonton, AB"
+              outlined
+            >
+            </v-text-field>
+          </v-col>
+          <v-row class="justify-end">
+            <v-btn class="text-capitalize text-h6" width="150" height="40"
+              >Cancel
+            </v-btn>
+
+            <v-btn
+              class="primary text-capitalize text-h6 ml-5 mr-5"
+              width="150"
+              height="40"
+            >
+              Submit
+            </v-btn>
+          </v-row>
+        </v-row>
+      </v-card>
     </v-row>
 
-    <h2>{{ name.firstName }}</h2>
+    <!-- <h2>{{ name.firstName }}</h2> -->
   </v-container>
 </template>
 
@@ -17,15 +81,21 @@ export default {
     error: "",
     name: [],
   }),
-  mounted() {
-    fetch(GET_URL)
-      .then((response) => response.json())
-      .then((data) => {
-        {
-          console.log(data);
-          this.name = data[0];
-        }
-      });
-  },
+  // mounted() {
+  //   fetch(GET_URL)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       {
+  //         console.log(data);
+  //         this.name = data[0];
+  //       }
+  //     });
+  // },
 };
 </script>
+
+<style>
+.container {
+  margin: auto;
+}
+</style>
--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const emailRegEx = new RegExp(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/);
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -8,18 +9,49 @@ const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
     required: true,
+    minlength: [1, "First name must be 1 character or more."],
+    maxlength: [30, "First name cannot be greater than 30 characters."],
   },
   lastName: {
     type: String,
     required: true,
+    minlength: [1, "Last name must be 1 character or more."],
+    maxlength: [30, "Last name cannot be greater than 30 characters."],
   },
   email: {
     type: String,
     required: true,
+    match: [emailRegEx, "Email must be in the format: fyf@gmail.com"],
+    trim: true,
   },
   password: {
     type: String,
     required: true,
+    minlength: [8, "Password must contain 8 or more characters."],
+    trim: true,
+  },
+  gender: {
+    type: String,
+    required: false,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  birthday: {
+    type: Date,
+    min: "1900-01-01",
+    max: Date.now,
+    required: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  subscribedToEmail: {
+    type: Boolean,
+    required: false,
+    default: false,
   },
 });
 
