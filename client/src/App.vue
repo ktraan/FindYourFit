1bdf9b59516be916d9d0f46fb203eeea60dcced4
ktran <kevintraan@live.com>
2020-10-26 23:00:28 -0600

    Home Page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <v-app>
-    <v-app-bar class="d-flex justify-space-around" max-height="70" >
+    <HomePageHeader v-if="isHome" />
+    <v-app-bar
+      v-if="isNotHome"
+      class="d-flex justify-space-around"
+      max-height="70"
+    >
       <router-link to="/">Home</router-link>
       <router-link to="/register">Register</router-link>
       <router-link to="/login">Login</router-link>
@@ -12,11 +17,22 @@
 </template>
 
 <script>
+import HomePageHeader from "../src/components/HomePageHeader";
+
 export default {
   name: "App",
+  components: { HomePageHeader },
 
   data: () => ({
     //
   }),
+  computed: {
+    isNotHome() {
+      return this.$route.name !== "home";
+    },
+    isHome() {
+      return this.$route.name === "home";
+    },
+  },
 };
 </script>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,5 +1,33 @@
-<template> 
-  <h1>Home Page</h1>
+<template>
+  <v-container fluid>
+    <v-spacer></v-spacer>
+    <v-row class=" justify-space-between">
+      <v-img src="../..//public/yoga.jpg" max-height="500" max-width="700">
+      </v-img>
+
+      <v-col cols="6" class="mt-auto">
+        <div class="text-h1 ml-3">
+          Find Your Fit
+        </div>
+        <div class="text-h5 mb-5 ml-16">
+          A tool to help you find and connect with trainers to meet your health
+          & fitness ambitions.
+        </div>
+      </v-col>
+      <v-row class="justify-end">
+        <v-col cols="6" class="mt-auto">
+          <div class="text-lg-h4 pb-6">
+            Finding the right fitness expert to meet your needs can be difficult
+            sometimes. Our mission is to help you find a health or fitness
+            professional to meet your goals effortlessly.
+          </div>
+        </v-col>
+        <v-img src="../../public/stairs.jpg" max-width="700"></v-img
+      ></v-row>
+    </v-row>
+    <v-row></v-row>
+    <v-row></v-row>
+  </v-container>
 </template>
 
 <script>
@@ -9,3 +37,9 @@ export default {
   components: {},
 };
 </script>
+
+<style>
+.container {
+  background-color: #dbd9d9;
+}
+</style>
