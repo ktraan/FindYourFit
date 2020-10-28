f284528edf25bc37b4ca626d72a80ad406e68f4d
Kevin Tran <kevintraan@live.com>
2020-10-27 19:20:30 -0600

    Fixed merge conflicts, styling Home page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,38 @@
 <template>
   <v-app>
-    <Header />
-    
+    <HomePageHeader v-if="isHome" />
+    <v-app-bar
+      v-if="isNotHome"
+      class="d-flex justify-space-around"
+      max-height="70"
+    >
+      <router-link to="/">Home</router-link>
+      <router-link to="/register">Register</router-link>
+      <router-link to="/login">Login</router-link>
+    </v-app-bar>
     <v-main>
-      <v-icon>fas fa-lock </v-icon>
       <router-view />
-      
     </v-main>
     
   </v-app>
 </template>
 
 <script>
-import Header from '../src/components/Header'
+import HomePageHeader from '../src/components/HomePageHeader'
+
 export default {
   name: "App",
-  components: { Header },
+  components: { HomePageHeader },
   data: () => ({
     //
   }),
+    computed: {
+    isNotHome() {
+      return this.$route.name !== "home";
+    },
+    isHome() {
+      return this.$route.name === "home";
+    },
+  },
 };
 </script>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,27 +2,27 @@
   <v-container fluid>
     <v-spacer></v-spacer>
     <v-row class=" justify-space-between">
-      <v-img src="../..//public/yoga.jpg" max-height="500" max-width="700">
+      <v-img src="../..//public/yoga.jpg" max-width="900">
       </v-img>
 
-      <v-col cols="6" class="mt-auto">
-        <div class="text-h1 ml-3">
+      <v-col cols="6" class="mt-auto mb-10">
+        <h1 class="h1-text  font-weight-regular">
           Find Your Fit
-        </div>
-        <div class="text-h5 mb-5 ml-16">
+        </h1>
+        <h2 class="mb-5 ml-10 font-weight-medium font-italic">
           A tool to help you find and connect with trainers to meet your health
           & fitness ambitions.
-        </div>
+        </h2>
       </v-col>
       <v-row class="justify-end">
-        <v-col cols="6" class="mt-auto">
-          <div class="text-lg-h4 pb-6">
+        <v-col cols="6" class="mt-auto mb-auto">
+          <div class="description-text pb-6 font-weight-medium">
             Finding the right fitness expert to meet your needs can be difficult
             sometimes. Our mission is to help you find a health or fitness
             professional to meet your goals effortlessly.
           </div>
         </v-col>
-        <v-img src="../../public/stairs.jpg" max-width="700"></v-img
+        <v-img src="../../public/stairs.jpg" max-width="950"></v-img
       ></v-row>
     </v-row>
     <v-row></v-row>
@@ -41,5 +41,12 @@ export default {
 <style>
 .container {
   background-color: #dbd9d9;
+  font-family: Arial, Helvetica, sans-serif;
+}
+.h1-text{
+  font-size: 100px;
+}
+.description-text{
+  font-size: 35px;
 }
 </style>
