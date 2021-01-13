5bbb4ad8844bee1d8eb642c76f07494aff3675c3
Kevin Tran <kevintraan@live.com>
2021-01-13 11:00:32 -0700

    listing card component

--- client/src/views/Listings.vue
+++ client/src/views/Listings.vue
@@ -1,12 +1,36 @@
 <template>
   <v-container>
-    <h1>All Listings</h1>
-    <v-card v-for="listing in listings" :key="listing.id">
-      <v-card-title>
-        {{ `${listing.creator.firstName} ${listing.creator.lastName}` }}
-      </v-card-title>
-    </v-card>
-    <!-- <v-btn @click="getAllListings">Get Listings</v-btn> -->
+    <v-row class="mt-10"></v-row>
+    <v-row justify="center">
+      <v-card
+        v-for="listing in listings"
+        :key="listing.id"
+        width="400"
+        class="ml-16"
+      >
+        <v-img :src="listing.profilePicture" max-width="400"></v-img>
+        <v-card-title class="text-h4 justify-center">
+          {{ `${listing.creator.firstName} ${listing.creator.lastName}` }}
+        </v-card-title>
+
+        <!-- Icons -->
+        <v-row justify="center" class="mt-n2">
+          <v-btn class="" icon>
+            <v-icon color="blue darken-2" large>mdi-facebook</v-icon>
+          </v-btn>
+          <v-btn class="ml-4" icon>
+            <v-icon color="black" large>mdi-instagram</v-icon>
+          </v-btn>
+          <v-btn class="ml-4" icon>
+            <v-icon color="red" large>mdi-youtube</v-icon>
+          </v-btn>
+          <v-btn class="ml-4" icon>
+            <v-icon color="blue darken-2" large>mdi-twitter</v-icon>
+          </v-btn>
+        </v-row>
+        <v-divider></v-divider>
+      </v-card>
+    </v-row>
 
     <v-btn @click="logger">Logger</v-btn>
   </v-container>
@@ -30,7 +54,13 @@ export default {
       errors: "",
       creators: [],
       listings: [],
-      listingCount: 0
+
+      icons: [
+        { facebook: "mdi-facebook" },
+        { instagram: "mdi-instagram" },
+        { instagram: "mdi-youtube" },
+        { instagram: "mdi-twitter" }
+      ]
     };
   },
   mounted() {
