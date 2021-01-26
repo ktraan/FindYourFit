f89de9e79c7ac77d524997c1a7bf6729cc71636b
Kevin Tran <kevintraan@live.com>
2021-01-26 13:45:18 -0700

    ViewListing Styling

--- client/src/views/ViewListing.vue
+++ client/src/views/ViewListing.vue
@@ -3,73 +3,90 @@
     <v-alert v-if="errors" border="bottom" colored-border type="error">
       {{ errors }}
     </v-alert>
-    <v-card class="mt-5">
-      <v-row>
-        <v-col cols="4">
-          <v-img
-            :lazy-src="listing.profilePicture"
-            :src="listing.profilePicture"
-            transition="fade-transition"
-            max-width="500"
-          ></v-img>
-        </v-col>
-        <v-col cols="6" class="ml-xl-n10">
-          <v-row>
-            <v-card-title
-              class="text-h5 text-sm-h3 text-md-h2 mb-2 font-weight-light"
+    <v-row justify="center">
+      <v-card class="mt-12" max-width="1400">
+        <v-row>
+          <v-col cols="4">
+            <v-img
+              :lazy-src="listing.profilePicture"
+              :src="listing.profilePicture"
+              transition="fade-transition"
+              max-width="600"
+            ></v-img>
+          </v-col>
+          <v-col md="3" class="">
+            <v-row>
+              <v-card-title
+                class="text-h5 text-sm-h3 text-md-h2 mb-2 font-weight-light"
+              >
+                {{ fullName }}
+              </v-card-title>
+            </v-row>
+            <v-row
+              class="ml-2 text-h6 text-sm-h5 text-md-h4 font-weight-regular mb-2"
             >
-              {{ fullName }}
-            </v-card-title>
-          </v-row>
-          <v-row class="ml-2">
-            <v-icon color="black" x-large>mdi-phone</v-icon>
-            <div
-              class="text-h7 text-sm-h6 text-md-h5 font-weight-light mt-1 ml-5"
-            >
-              {{ listing.phone }}
-            </div>
-          </v-row>
-          <v-row class="ml-2 mt-2">
-            <v-icon color="black" x-large>mdi-email</v-icon>
-            <div
-              class="text-h7 text-sm-h6 text-md-h5 font-weight-light mt-1 ml-5"
-            >
-              {{ listing.email }}
-            </div>
-          </v-row>
-          <v-row class=" mt-2">
-            <v-btn
-              v-for="icon in icons"
-              :key="icon.id"
-              icon
-              :color="icon.color"
-              class="ml-5"
-            >
-              <v-icon class="ml-2" x-large>{{ icon.name }}</v-icon>
-            </v-btn>
-          </v-row>
-          <v-row>
-            <div
-              class="text-h7 text-sm-h6 text-md-h5 font-weight-regular ml-5 mt-5 "
-            >
-              {{ listing.summary }}
-            </div>
-          </v-row>
-          <v-row>
-            <div
-              class="text-h7 text-sm-h6 text-md-h5 font-weight-medium mt-1 ml-5"
-            >
-              {{ listing.yearsExperience }} years of experience
-            </div>
-          </v-row>
-          <v-row>
-            <div>
-              {{ listing.education }}
-            </div>
-          </v-row>
-        </v-col>
-      </v-row>
-    </v-card>
+              <div>{{ listing.occupation }}</div>
+            </v-row>
+            <v-row class="ml-2">
+              <v-icon color="black" x-large>mdi-phone</v-icon>
+              <div
+                class="text-h7 text-sm-h6 text-md-h5 font-weight-light mt-1 ml-5"
+              >
+                {{ listing.phone }}
+              </div>
+            </v-row>
+            <v-row class="ml-2 mt-2">
+              <v-icon color="black" x-large>mdi-email</v-icon>
+              <div
+                class="text-h7 text-sm-h6 text-md-h5 font-weight-light mt-1 ml-5"
+              >
+                {{ listing.email }}
+              </div>
+            </v-row>
+            <v-row class="mt-3">
+              <v-btn
+                v-for="icon in icons"
+                :key="icon.id"
+                icon
+                :color="icon.color"
+                class="ml-5"
+              >
+                <v-icon class="ml-2" x-large>{{ icon.name }}</v-icon>
+              </v-btn>
+            </v-row>
+            <v-row>
+              <div
+                class="text-h7 text-sm-h6 text-md-h5 font-weight-regular mt-3 ml-5"
+              >
+                - {{ listing.yearsExperience }} years of experience
+              </div>
+            </v-row>
+            <v-row>
+              <v-chip
+                class="text-h7 text-sm-h6 font-weight-regular mt-3 ml-5"
+                v-for="item in listing.education"
+                :key="item.id"
+                color="amber darken-2"
+                outlined
+                medium
+              >
+                <v-icon class="mr-2" left>mdi-school</v-icon>
+                {{ item }}
+              </v-chip>
+            </v-row>
+          </v-col>
+          <v-col md="4" class="">
+            <v-row class="mt-xl-16 mt-lg-5">
+              <div
+                class="text-h7 text-sm-h6 text-md-h5 text-lg-h4 font-weight-regular mt-10 mt-lg-0 mt-xl-5 ml-5"
+              >
+                {{ listing.summary }}
+              </div>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-card></v-row
+    >
     <v-row justify="center">
       <v-btn
         class="mt-5 amber darken-2 text-h6 text-capitalize font-weight-regular"
