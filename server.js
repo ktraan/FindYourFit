b02fe4d5dabfec7ebeb73b79bfc682064bdfe078
Kevin Tran <kevintraan@live.com>
2021-02-17 22:25:03 -0700

    .

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
+// Load out .env file if we are in development mode
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const cors = require('cors');
 const history = require('connect-history-api-fallback');
+
+// Routes
+const userRouter = require('../FindYourFit/server/routes/user');
+const registerRouter = require('../FindYourFit/server/routes/register');
+const loginRouter = require('../FindYourFit/server/routes/login');
+const listingRouter = require('../FindYourFit/server/routes/listing');
+
+// Configuring port
+const port = process.env.PORT || 3000;
 
 const app = express();
 const staticFileMiddleware = express.static(
@@ -15,7 +33,41 @@ app.get('/', function (req, res) {
   res.render(path.join(__dirname + '/client/dist/index.html'));
 });
 
-app.listen(5000, function () {
-  console.log('Listening on 5000');
-  console.log(__dirname + 'client/dist/index.html');
+// Configure middlewares
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(
+  session({
+    secret: 'keyboard dog',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(express.urlencoded({ extended: true }));
+
+// Database connection
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+const db = mongoose.connection;
+db.on('error', (error) => {
+  console.error(error);
+});
+db.once('open', () => {
+  console.log(`Connected to Database`);
+});
+
+// Defining route middleware
+app.use('/user', userRouter);
+app.use('/register', registerRouter);
+app.use('/login', loginRouter);
+app.use('/listing', listingRouter);
+
+// Listening to port
+app.listen(port);
+console.log(`Listening On http://localhost:${port}`);
+
+module.exports = app;
