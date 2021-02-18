599290bfbcad779cb2a9a2a4f867f1b116e0798b
Kevin Tran <kevintraan@live.com>
2021-02-17 22:17:05 -0700

    .

--- server.js
+++ server.js
@@ -3,16 +3,19 @@ const path = require('path');
 const history = require('connect-history-api-fallback');
 
 const app = express();
-const staticFileMiddleware = express.static(path.join(__dirname));
+const staticFileMiddleware = express.static(
+  path.join(__dirname + 'client/dist')
+);
 
 app.use(staticFileMiddleware);
 app.use(history());
 app.use(staticFileMiddleware);
 
 app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '/index.html'));
+  res.render(path.join(__dirname + '/client/dist/index.html'));
 });
 
 app.listen(5000, function () {
   console.log('Listening on 5000');
+  console.log(__dirname + 'client/dist/index.html');
 });
